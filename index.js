fd045152ad8f7de99a6845f69e6f12211835aabd
Narcis2005 <narcischirilov@gmail.com>
2021-12-04 17:19:11 +0200

    linked api

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 var toastTrigger = document.getElementById('liveToastBtn')
 var toastLiveExample = document.getElementById('liveToast')
+const qrSection = document.getElementById("qr-section");
+const qrCodeImg = document.getElementById("qr");
+const downloadButton = document.getElementById("download-button");
 if (toastTrigger) {
   toastTrigger.addEventListener('click', function () {
     var toast = new bootstrap.Toast(toastLiveExample)
 
     toast.show()
+    setTimeout( () => {
+      axios.get("http://localhost:4000/get-code")
+      .then(data => {
+        qrCodeImg.src = data.data.url;
+        qrSection.style.display = "block";
+        downloadButton.href = data.data.url;
+        console.log(data.data.id)
+      })
+      .catch(err => {
+        console.error(err);
+        alert("error")
+      })
+    }, 1000)
+    
   })
 }
