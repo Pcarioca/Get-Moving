b9bc1da77ff708fbd40fc6cba964e0c0d21ce436
Andrei Munteanu <63117204+Pcarioca@users.noreply.github.com>
2021-12-04 17:34:00 +0200

    made constants for all the inputs

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ var toastLiveExample = document.getElementById('liveToast')
 const qrSection = document.getElementById("qr-section");
 const qrCodeImg = document.getElementById("qr");
 const downloadButton = document.getElementById("download-button");
+const TransportOption = document.getElementsByClassName("transport");
+const validationCustom1 = document.getElementById("validationCustom01"); //prenume
+const validationCustom2 = document.getElementById("validationCustom02"); //nume
+const validationCustom3 = document.getElementById("validationCustom03"); //city
+const validationCustom4 = document.getElementById("validationCustom04"); //county
+const validationCustom5 = document.getElementById("validationCustom05"); //zip code
+const email = document.getElementById("email"); //example
+const at = document.getElementById("at"); //at
+const adress = document.getElementById("address"); //adresa
+const prefix = document.getElementById("floatingSelectGrid")  //prefix
+const phone = document.getElementById("floatingInputGrid"); //phone number
+const terms = document.getElementById("invalidCheck"); //terms and conditions
+
+
 if (toastTrigger) {
   toastTrigger.addEventListener('click', function () {
     var toast = new bootstrap.Toast(toastLiveExample)
